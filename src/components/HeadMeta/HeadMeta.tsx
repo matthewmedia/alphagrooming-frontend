6eab35ebbc15c2e4bdc46e759b72d509c85f2e48
matthewmedia <matthewmanning432@gmail.com>
2023-12-21 06:28:45 -0500

    chore: fixes the app/layout

--- src/components/HeadMeta/HeadMeta.tsx
+++ src/components/HeadMeta/HeadMeta.tsx
@@ -36,6 +36,12 @@ const HeadMeta = () => {
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <title>AlphaGrooming</title>
 
+            <link rel="stylesheet" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/css/fontawesome-all.min.css`} />
+            <link rel="stylesheet" href={`${process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''}/css/iconfont.css`} />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+
 
             <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
