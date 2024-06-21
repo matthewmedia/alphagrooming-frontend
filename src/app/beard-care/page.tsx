18749cb5d45cf806d65f5b37d6af5bc0ce9b697c
matthewmedia <matthewmanning432@gmail.com>
2024-06-21 00:44:19 -0400

    New Layout/Style For Beard Care

--- src/app/beard-care/page.tsx
+++ src/app/beard-care/page.tsx
@@ -1,31 +1,43 @@
 import Layout from "@/components/Layout/Layout";
 import HeadMeta from "@/components/HeadMeta/HeadMeta";
 import { loadQuery } from "@/sanity/lib/store";
-import {
-  POSTS_QUERY,
-} from "@/sanity/lib/queries";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
 import { SanityDocument } from "next-sanity";
 import BeardCategory from "@/components/BeardCategory/BeardCategory";
 
-
-
-
+import Image from "next/image";
+import LayoutCards from "@/components/PostLayoutOne/LayoutHorizontalCards";
 
 export default async function BeardCare() {
   const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY);
- 
+
   return (
     <>
       <HeadMeta />
       <Layout style={{ backgroundImage: `url('/background.png')` }}>
         <div
-          className="bg-gradient-to-r from-green-400 to-blue-500 border rounded-3xl shadow-lg p-16 mb-36"
+          className="bg-gradient-to-r from-green-400 to-blue-500 border rounded-3xl shadow-lg "
           style={{ backgroundImage: `url('/shaving-brush-bg.jpeg')` }}
         >
-            <div className="flex items-center justify-left space-x-16">
-            <BeardCategory posts={initial.data} /> 
+          <div className="flex flex-col items-center justify-center space-y-2 text-center bg-gradient-to-r from-green-400 to-blue-500  mt-24 md:mt-32 lg:mt-48 xl:mt-64 2xl:mt-96   rounded-3xl shadow-2xl	p-16	">
+            <div>
+              <h1 className="text-5xl text-white font-bold text-center  mb-[0.1rem] ">
+                Beard Care
+              </h1>
+              <h2 className="text-2xl text-white font-thin">
+                Be the Beard Boss
+              </h2>
             </div>
+            <Image
+              src="/beard-graphic.gif"
+              alt="Beard Care"
+              width={200}
+              height={200}
+              className="rounded-full"
+            />
+          </div>
         </div>
+        <LayoutCards posts={initial.data} hideCategoryHeader />
       </Layout>
     </>
   );
--- src/components/PostLayoutOne/LayoutHorizontalCards.tsx
+++ src/components/PostLayoutOne/LayoutHorizontalCards.tsx
@@ -1,35 +1,41 @@
 "use client";
-import Image from "next/image";
-import { FaArrowRightLong } from "react-icons/fa6";
-// star icon
+
 import { FaStar } from "react-icons/fa";
 import { SanityDocument } from "next-sanity";
 import Link from "next/link";
 import React from "react";
 import CardLayoutOne from "./CardLayoutOne";
-import { Button } from "react-bootstrap";
 
-const LayoutCards = ({ posts }: { posts: SanityDocument[] }) => {
+const LayoutCards = ({
+  posts,
+  hideCategoryHeader,
+}: {
+  posts: SanityDocument[];
+  hideCategoryHeader?: boolean;
+}) => {
   return (
     <>
       <div className="container mx-auto pt-10 pb-16 lg:pb-28 lg:pt-20 space-y-16 lg:space-y-28">
-        {
+        {!hideCategoryHeader && (
           <Link href={`/beard-care`}>
-          <div className="post-cat-group m-b-xs-10 rounded-3xl ">
-            <div className={` p-6 bg-green-400 rounded-3xl flex items-center space-x-2 underline decoration-dotted underline-offset-8 decoration-white`}>
-              <span className="text-white text-3xl font-bold">Beard Care</span>
-              <FaStar className="text-yellow-400 animate-bounce" />
+            <div className="post-cat-group m-b-xs-10 rounded-3xl ">
+              <div
+                className={` p-6 bg-green-400 rounded-3xl flex items-center space-x-2 underline decoration-dotted underline-offset-8 decoration-white`}
+              >
+                <span className="text-white text-3xl font-bold">
+                  Beard Care
+                </span>
+                <FaStar className="text-yellow-400 animate-bounce" />
+              </div>
             </div>
-          </div>
-        </Link>
-        }
+          </Link>
+        )}
         <div className="grid justify-items-center  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8 mt-8 lg:mt-10 ">
           {posts.map((post, _index) => (
             <CardLayoutOne post={post} key={_index} />
           ))}
         </div>
       </div>
-      
     </>
   );
 };
